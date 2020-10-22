05f206acde006be86429d01ce6e1ccc974eb4e1f
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-10-22 18:07:00 +0300

    milestone: dynamically generated new book form + new book form adds new book to myLibrary[]

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const book1 = new Book(`The Fellowship of the Ring`, `J.R.R. Tolkien`, 423, `Rea
 const book2 = new Book(`The Two Towers`, `J.R.R. Tolkien`, 352, `Not Read`);
 const book3 = new Book(`The Return of the King`, `J.R.R. Tolkien`, 416, `Not Read`);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+addBookToLibraryFiller(book1);
+addBookToLibraryFiller(book2);
+addBookToLibraryFiller(book3);
 
-document.querySelector(`.floating-button`).addEventListener('click', newBook);
+document.querySelector(`.floating-button`).addEventListener('click', generateNewBookForm);
 
 function Book(title, author, numPages, status) {
     this.title = title;
@@ -17,7 +17,7 @@ function Book(title, author, numPages, status) {
     this.status = status;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibraryFiller(book) {
     myLibrary.push(book);
 }
 
@@ -149,8 +149,144 @@ function clearCardList() {
     cardList.innerHTML = ``;
 }
 
-function newBook() {
+function generateNewBookForm() {
+    let libraryBody = document.body;
+    let container = document.querySelector('#container');
+    container.classList.add(`blur`, `unclickable`);
+    let floatingButton = document.querySelector('.floating-button');
+    floatingButton.classList.add(`hidden`);
+    let newBookForm = document.createElement('form');
+    newBookForm.classList.add(`new-book-form`);
 
+    let newBookFormHeader = document.createElement('div');
+    newBookFormHeader.classList.add(`new-book-form-header`);
+    newBookFormHeader.innerHTML = `Add a New Book`;
+    newBookForm.appendChild(newBookFormHeader);
+    let newBookFormLineTop = document.createElement('div');
+    newBookFormLineTop.classList.add(`line`);
+    newBookForm.appendChild(newBookFormLineTop);
+
+    let newBookTitle = document.createElement('div');
+    newBookTitle.classList.add(`new-book-title`, `new-book-field`);
+    let newBookTitleText = document.createElement('label');
+    newBookTitleText.classList.add(`new-book-form-field-text`);
+    newBookTitleText.textContent = `Title`;
+    newBookTitle.appendChild(newBookTitleText);
+    let newBookTitleInput = document.createElement('input');
+    newBookTitleInput.setAttribute('type', 'text');
+    newBookTitleInput.classList.add(`new-book-input`, `new-title-input`);
+    newBookTitle.appendChild(newBookTitleInput);
+    newBookForm.appendChild(newBookTitle);
+
+    let newBookFormLineMiddleTop = document.createElement('div');
+    newBookFormLineMiddleTop.classList.add(`line`);
+    newBookForm.appendChild(newBookFormLineMiddleTop);
+
+    let newBookAuthor = document.createElement('div');
+    newBookAuthor.classList.add(`new-book-form-author`, `new-book-field`);
+    let newBookAuthorText = document.createElement('label');
+    newBookAuthorText.classList.add(`new-book-form-field-text`);
+    newBookAuthorText.textContent = `Author`;
+    newBookAuthor.appendChild(newBookAuthorText);
+    let newBookAuthorInput = document.createElement('input');
+    newBookAuthorInput.setAttribute('type', 'text');
+    newBookAuthorInput.classList.add(`new-book-input`, `new-author-input`);
+    newBookAuthor.appendChild(newBookAuthorInput);
+    newBookForm.appendChild(newBookAuthor);
+
+    let newBookFormLineMiddleBottom = document.createElement('div');
+    newBookFormLineMiddleBottom.classList.add(`line`);
+    newBookForm.appendChild(newBookFormLineMiddleBottom);
+
+    let newBookPagesAndStatus = document.createElement('div');
+    newBookPagesAndStatus.classList.add(`new-book-pages-n-status`, `new-book-field`);
+    let newBookPagesText = document.createElement('label');
+    newBookPagesText.classList.add(`new-book-form-field-text`);
+    newBookPagesText.textContent = `Pages`;
+    newBookPagesAndStatus.appendChild(newBookPagesText);
+    let newBookPagesSpinner = document.createElement('input');
+    newBookPagesSpinner = setSpinnerAttributes(newBookPagesSpinner, {'type':'number', 'min':'1', 'max':'1000'});
+    newBookPagesSpinner.classList.add(`pages-spinner`);
+    let newBookStatusText = document.createElement('label');
+    newBookStatusText.classList.add(`new-book-form-field-text`);
+    newBookStatusText.textContent = `Status`;
+    let newBookStatusDropdown = document.createElement('select');
+    newBookStatusDropdown.classList.add(`status-select`);
+    let statusSelectRead = document.createElement('option');
+    statusSelectRead.setAttribute('value', 'Read');
+    statusSelectRead.classList.add(`read`);
+    statusSelectRead.innerHTML = `Read`;
+    newBookStatusDropdown.appendChild(statusSelectRead);
+    let statusSelectNotRead = document.createElement('option');
+    statusSelectNotRead.setAttribute('value', 'Not Read');
+    statusSelectNotRead.classList.add(`not-read`);
+    statusSelectNotRead.innerHTML = `Not Read`;
+    newBookStatusDropdown.appendChild(statusSelectNotRead);
+
+    newBookPagesAndStatus.appendChild(newBookPagesText);
+    newBookPagesAndStatus.appendChild(newBookPagesSpinner);
+    newBookPagesAndStatus.appendChild(newBookStatusText);
+    newBookPagesAndStatus.appendChild(newBookStatusDropdown);
+    newBookForm.appendChild(newBookPagesAndStatus);
+
+    let newBookFormLineBottom = document.createElement('div');
+    newBookFormLineBottom.classList.add(`line`);
+    newBookForm.appendChild(newBookFormLineBottom);
+
+    let newBookFormButtons = document.createElement('div');
+    newBookFormButtons.classList.add(`new-book-form-buttons`);
+    let submitButton = document.createElement('input');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.setAttribute('value', 'Submit');
+    submitButton.classList.add(`submit-button`, `form-button`);
+    newBookFormButtons.appendChild(submitButton);
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add(`cancel-button`, `form-button`);
+    cancelButton.innerHTML = `Cancel`;
+    cancelButton.addEventListener('click', closeForm);
+    newBookFormButtons.appendChild(cancelButton);
+    newBookForm.appendChild(newBookFormButtons);
+
+    libraryBody.appendChild(newBookForm);
+
+    submitButton.addEventListener('click', addBookToLibrary);
+}
+
+function closeForm() {
+    let select = document.body;
+    select.removeChild(select.lastChild);
+
+    let container = document.querySelector('#container');
+    container.classList.remove(`blur`, `unclickable`);
+
+    let floatingButton = document.querySelector('.floating-button');
+    floatingButton.classList.remove(`hidden`);
+}
+
+function setSpinnerAttributes(spinner, attrs) {
+    for (var key in attrs) {
+        spinner.setAttribute(key, attrs[key]);
+    }
+
+    return spinner;
+}
+
+function addBookToLibrary() {
+    let newBookTitle = document.querySelector('.new-title-input').value;
+    let newBookAuthor = document.querySelector('.new-author-input').value;
+    let newBookNumPages = document.querySelector('.pages-spinner').value;
+    let newBookStatus = document.querySelector('.status-select').value;
+
+    if(newBookTitle == "" || newBookAuthor == "" || newBookNumPages == "" || newBookStatus == "") {
+        alert(`Please fill out all the fields in the form!`);
+    } else {
+        let newBook = new Book(newBookTitle, newBookAuthor, newBookNumPages, newBookStatus);
+        console.table(newBook);
+        myLibrary.push(newBook);
+    }
+    closeForm();
+    clearCardList();
+    displayBooks(myLibrary);
 }
 
 displayBooks(myLibrary);
